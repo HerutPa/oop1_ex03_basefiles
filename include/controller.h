718b38714482ab0c54cfc9cd8198ebfcbd2fc928
Herut Partner <herutpa@std.hac.ac.il>
2024-02-01 10:45:21 +0200

    dfdsf

--- include/controller.h
+++ include/controller.h
@@ -1,37 +1,37 @@
-//#pragma once
-//
-//#include <SFML/Graphics.hpp>
-//#include <vector>
-////#include "Board.h"
-////#include "ToolBar.h"
-//#include <fstream>
-//
-//
-//class Controller
-//{
-//public:
-//    Controller();
-//    void run();
-//
-//    void init();
-//
-//
-//private:
-//
-//    //members
-//    sf::RenderWindow m_window;
-//    std::vector< std::vector <char> > m_matrix;
-//    Board m_board;
-//    ToolBar m_ToolBar;
-//    sf::Texture m_background;
-//    std::ifstream m_ifile = std::ifstream("Board.txt");
-//    std::ofstream m_ofile;
-//
-//
-//
-//
-//
-//
-//
-//
-//};
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+//#include "Board.h"
+//#include "ToolBar.h"
+#include <fstream>
+
+
+class Controller
+{
+public:
+    Controller();
+    void run();
+
+    void init();
+
+
+private:
+
+    //members
+    sf::RenderWindow m_window;
+    std::vector< std::vector <char> > m_matrix;
+    //Board m_board;
+    //ToolBar m_ToolBar;
+    sf::Texture m_background;
+    std::ifstream m_ifile = std::ifstream("Board.txt");
+    std::ofstream m_ofile;
+
+
+
+
+
+
+
+
+};
--- src/controller.cpp
+++ src/controller.cpp
@@ -1,26 +1,25 @@
-//#include "Controller.h"
-//
-////constructor
-//Controller::Controller()
-//    :m_window(sf::VideoMode(1200, 900), "Mouse&Cat Editor")
-//{
-//    m_background.loadFromFile("background.png");
-//    m_background.setSmooth(true);
-//    sf::Sprite s1(m_background);
-//    s1.scale(sf::Vector2f(1.75f, 1.75f));
-//    m_window.clear(sf::Color::Color(255, 255, 230));
-//    m_window.draw(s1);
-//    m_window.display();
-//}
-//
-//
+#include "Controller.h"
+
+//constructor
+Controller::Controller()
+    :m_window(sf::VideoMode(1200, 900), "Mouse&Cat Editor")
+{
+    m_background.loadFromFile("background.png");
+    m_background.setSmooth(true);
+    sf::Sprite s1(m_background);
+    s1.scale(sf::Vector2f(1.75f, 1.75f));
+    m_window.clear(sf::Color::Color(255, 255, 230));
+    m_window.draw(s1);
+    m_window.display();
+}
+
+
 //void Controller::run()
 //{
 //    //sets the background sprit
 //    sf::Sprite s1(m_background);
 //    s1.scale(sf::Vector2f(1.75f, 1.75f));
 //    init();
-//
 //}
 //
 //
